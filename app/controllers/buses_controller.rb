4b9bfc69b6abd87824413824ddd4dd8fc1e4b613
Jesse Shawl <jesse@jshawl.com>
2015-05-23 03:23:09 -0400

    allow users to create new buses

---
+++ app/controllers/buses_controller.rb
@@ -0,0 +1,37 @@
+class BusesController < ApplicationController
+  def show
+    @bus = Bus.find(params[:id])
+  end
+  def edit
+    @bus = Bus.find(params[:id])
+  end
+  def update
+    @bus = Bus.find(params[:id])
+    if @bus.update bus_params
+      redirect_to @bus
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @bus = Bus.find(params[:id])
+    @bus.destroy
+    redirect_to buses_path
+  end
+  def new
+    @bus = Bus.new
+  end
+  def create
+    @bus = Bus.new bus_params
+    if @bus.save
+      redirect_to @bus
+    else
+      render :new
+    end
+  end
+
+  private
+  def bus_params
+    params.require(:bus).permit(:name, :pick_up, :distance, :date, :drive_time, :total_time)
+  end
+end
--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,2 +1,3 @@
 class Bus < ActiveRecord::Base
+
 end
